e9028d66e10573f3c157d17cb6a1d6d309ddd5f4
Łukasz Wasiluk <lukasz.wasiluk01@gmail.com>
2026-05-18 03:33:43 +0200

    feat: create statistics dashboard

---
+++ BookmarkManager/Controllers/StatisticsController.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookmarkManager.Data;
+using BookmarkManager.Models;
+
+namespace BookmarkManager.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StatisticsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var totalUsers = await _context.Users.CountAsync();
+            var totalBookmarks = await _context.Bookmarks.CountAsync();
+            var totalCategories = await _context.Categories.CountAsync();
+
+            var mostPopularCategory = await _context.Bookmarks
+                .Include(b => b.Category)
+                .GroupBy(b => b.Category.Name)
+                .OrderByDescending(g => g.Count())
+                .Select(g => g.Key)
+                .FirstOrDefaultAsync();
+
+            var mostActiveUser = await _context.Bookmarks
+                .Include(b => b.User)
+                .GroupBy(b => b.User.Email)
+                .OrderByDescending(g => g.Count())
+                .Select(g => g.Key)
+                .FirstOrDefaultAsync();
+
+            var viewModel = new StatisticsViewModel
+            {
+                TotalUsers = totalUsers,
+                TotalBookmarks = totalBookmarks,
+                TotalCategories = totalCategories,
+                MostPopularCategory = mostPopularCategory ?? "Brak danych",
+                MostActiveUser = mostActiveUser ?? "Brak danych"
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ BookmarkManager/Models/StatisticsViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace BookmarkManager.Models
+{
+    public class StatisticsViewModel
+    {
+        public int TotalUsers
+        {
+            get; set;
+        }
+        public int TotalBookmarks
+        {
+            get; set;
+        }
+        public int TotalCategories
+        {
+            get; set;
+        }
+        public string MostPopularCategory
+        {
+            get; set;
+        }
+        public string MostActiveUser
+        {
+            get; set;
+        }
+    }
+}
